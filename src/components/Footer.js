23b128be9dc70701f4b1c1d5f61dae1044caa8e2
MarcosLabra <mlabra.dev@gmail.com>
2022-02-26 11:09:11 -0300

    acomodo la carpeta de sass, divido en componentes y layout y agrego iconos de fontawsame en footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 const Footer = () => {
     return (
         <footer>
-            <p>Desarrollado por <span>Marcos Labra</span></p>
-            <ul>
-            <li><a href="https://www.facebook.com/corina.piercer.1">FB</a></li>
-            <li><a href="https://www.instagram.com/corinapiercer">INSTA</a></li>
-            <li><a href="https://wa.me/5492235592172">WAPP</a></li>
-            </ul>
-        </footer>
+        <ul>
+            <li class="footerCopy">Desarrollado por <p>Marcos Labra</p>
+            </li>
+            <li><a href="https://www.facebook.com/corina.piercer.1"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="https://www.instagram.com/corinapiercer"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://wa.me/5492235592172"><i class="fab fa-whatsapp"></i></a></li>
+        </ul>
+    </footer>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
-import "./style.scss"
+import "./sass/main.scss"
 
 ReactDOM.render(<App/>,document.getElementById("root"))
