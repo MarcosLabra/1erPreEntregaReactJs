bbcfc3f66957247a4fc01674415e59df3a736de6
MarcosLabra <mlabra.dev@gmail.com>
2022-03-12 14:41:01 -0300

    cambio titulo de la tienda

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom"
 const Header = () => {
   return (
     <header>
-        <NavLink to={"/"}><h1>Corina Piercer</h1></NavLink>
+        <NavLink to={"/"}><h1>Mi Tienda</h1></NavLink>
         <NavLink to="/cart"><CartWidget/></NavLink>
         <NavBar/>
     </header>
